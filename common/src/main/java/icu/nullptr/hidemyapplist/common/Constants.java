0386ddb3349f367458905dc2251be2eef6746d41
wangwang-code <64469117+wangwang-code@users.noreply.github.com>
2023-04-29 13:53:44 +0800

    Update Constants.java

--- common/src/main/java/icu/nullptr/hidemyapplist/common/Constants.java
+++ common/src/main/java/icu/nullptr/hidemyapplist/common/Constants.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 public class Constants {
 
-    public static final String APP_PACKAGE_NAME = "com.tsng.hidemyapplist";
+    public static final String APP_PACKAGE_NAME = "com.tsng.dingdingdog";
     public static final String GMS_PACKAGE_NAME = "com.google.android.gms";
     public static final String GSF_PACKAGE_NAME = "com.google.android.gsf";
     public static final String UPDATE_URL_BASE = "https://api.nullptr.icu/android/hide-my-applist/static/";
